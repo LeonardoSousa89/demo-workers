342a6613f577295d88a581898f52927c1a411901
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-05-22 18:07:14 -0300

    '@v1.1.4'

--- demo-workers-api/api/index.js
+++ demo-workers-api/api/index.js
@@ -1,16 +1,73 @@
 const db = require('../db/db')
+const{
+    exceptionFieldIsEmpty,
+    exceptionFieldNullOrUndefined,
+    exceptionFieldValueLessToType
+} = require('./error')
+
+const bcrypt = require('bcrypt')
 
 module.exports = app => {
 
-    
+    const cryptograph = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
+    }
 
-    const signUp = async(req, res) =>{
+
+
+    const signUp = async(req, res) => {
         
+        const data={
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        }
+
+        try{
+            exceptionFieldNullOrUndefined(data.username, 'username is undefined or null')
+            exceptionFieldIsEmpty(data.username.trim(), 'username can not be empty')
+            exceptionFieldNullOrUndefined(data.email, 'email is undefined or null')
+            exceptionFieldIsEmpty(data.email.trim(), 'email can not be empty')
+            exceptionFieldNullOrUndefined(data.password, 'password is undefined or null')
+            exceptionFieldIsEmpty(data.password.trim(), 'invalid password')
+            exceptionFieldValueLessToType(data.password.trim(), 'password must be greather than 4')
+          }catch(error){
+            return res.status(400)
+                      .json({
+                        error: error
+            })
+        }
+
+        const clientAlreadyExist=await knex_I.where('email', data.email)
+                                             .from('administrator')
+                                             .first()
+
+        if(clientAlreadyExist) return res.status(400)
+                                         .json({  msg: 'email already exists' }) 
+                                         
+        data.password=cryptograph(data.password)
+
+        await knex_I.insert(data)
+                    .from('administrator')
+                    .then(_=>{
+                            res.status(201)
+                               .json({
+                                msg: 'user created'
+                            })
+                     }).catch(_=>res.status(500)
+                                    .json({
+                                        msg: 'sorry, ocurred an error with server' 
+                     }))
     }
 
-    const signIn = async(req, res) =>{
+
+
+    const signIn = async(req, res) => {
 
     }
+
+
 
     const getData = async(req,res) => {
         await db('workeradmin')
@@ -18,6 +75,8 @@ module.exports = app => {
                     .then(data   => res.status(200).json(data))
                     .catch(err   => res.status(500).json(err) )
     }
+
+
 
     const getDataById = async(req,res) => {
         const { id } = req.params
@@ -28,9 +87,11 @@ module.exports = app => {
                              'workerjob',
                              'workersalary',
                              'workersector'])
-                    .then(data   => res.status(200).json(data))
-                    .catch(err   => res.status(500).json(err) )
+                    .then(data => res.status(200).json(data))
+                    .catch(err => res.status(500).json(err) )
     }
+
+
 
     const getDataByName = async(req,res) => {
         const { workername } = req.params
@@ -38,9 +99,11 @@ module.exports = app => {
         await db('workeradmin')
                         .where('workername', workername)
                         .select('*')
-                        .then(data   => res.status(200).json(data))
-                        .catch(err   => res.status(500).json(err) )
+                        .then(data => res.status(200).json(data))
+                        .catch(err => res.status(500).json(err) )
     }
+
+
 
     const postData = async(req,res) => {
         const { workername, workerjob, workersalary, workersector } = req.body
@@ -55,6 +118,8 @@ module.exports = app => {
                         .then(_      => res.status(201).json({msg: 'data inserted with success!'}))
                         .catch(err   => res.status(400).json(err) )
     }
+
+
 
     const putData = async(req,res) => {
         const { id } = req.query
@@ -72,6 +137,8 @@ module.exports = app => {
                         .catch(err   => res.status(400).json(err) )
     }
 
+
+
     const deleteData = async(req,res) => {
         const { id } = req.query
 
@@ -82,14 +149,15 @@ module.exports = app => {
                         .catch(err   => res.status(400).json(err) )    
     }
 
+    return {
+        signUp,
+        signIn,
+        getData , 
+        getDataById , 
+        getDataByName , 
+        postData , 
+        putData , 
+        deleteData 
+    }
 
-return {
-    signUp,
-    signIn,
-    getData , 
-    getDataById , 
-    getDataByName , 
-    postData , 
-    putData , 
-    deleteData }
 }
