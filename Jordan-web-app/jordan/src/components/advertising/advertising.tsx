ba2014477ce615e24266ef5abc207eab502a77d7
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-06-02 23:05:14 -0300

    '@v1.1.24'

--- Jordan-web-app/jordan/src/components/advertising/advertising.tsx
+++ Jordan-web-app/jordan/src/components/advertising/advertising.tsx
@@ -1,15 +1,26 @@
-import './advertising.css';
-import '../../App.css'
+import "./advertising.css";
+import "../../App.css";
 
-import Header from './header/header'
-import Footer from './footer/footer'
+import Notificacao from '../../assets/advertising/main/notificacao-de-lembrete.png'
+import Lista from '../../assets/advertising/main/lista-de-beneficios.png'
+
+import Header from "./header/header";
+import Footer from "./footer/footer";
 
 function Advertising() {
   return (
     <div className="advertising">
       <Header />
-      <div className='main'>
-        
+      <div className="main">
+        <div className="banner-agenda-info">
+          <div className="lista">
+            <img src={Lista} alt="lista de beneficios" />
+          </div>
+          <div className="notificacao">
+            <img src={Notificacao} alt="notificacao" />
+          </div>
+        </div>
+        <div className="banner-relatorio-xslx-pdf"></div>
       </div>
       <Footer />
     </div>
