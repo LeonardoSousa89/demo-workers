c34350c9f568932c1f6b857b9fbb1c00203fb1ed
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-06-03 21:25:06 -0300

    '@v1.1.26'

--- Jordan-web-app/jordan/src/components/advertising/footer/footer.tsx
+++ Jordan-web-app/jordan/src/components/advertising/footer/footer.tsx
@@ -1,12 +1,31 @@
-import './footer.css';
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import Play from '../../../assets/advertising/footer/pt-br_badge_web_generic (1).png'
+import Phone from '../../../assets/advertising/footer/5-smartphone-png-image 1.png'
+import Title from '../../../assets/advertising/footer/title.png'
+
+import "./footer.css";
 
 function Footer() {
-    return (
-      <div className="footer">
-        <div className="play-store">play store</div>
-        <div className="contact">contact</div>
+  return (
+    <div className="footer">
+      <div className="play-store">
+       <img className="play" 
+            src={Play} 
+            alt="play"
+            onClick={()=>window.open('https://pt.wix.com/website-template/view/html/1936?originUrl=https%3A%2F%2Fpt.wix.com%2Fwebsite%2Ftemplates&tpClick=view_button&esi=0a405a20-9139-4dea-9674-ffa0ef4a5b8b')} />
+       <img className="phone" src={Phone} alt="phone" />
       </div>
-    );
-  }
-  
-  export default Footer;
+      <div className="contact">
+        <div className="titulo">
+          <img className="title" src={Title} alt="titulo" />
+        </div>
+        <div className="form-social"></div>
+        <div className="copy"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
