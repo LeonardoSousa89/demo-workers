522533c17eae5d1fcbdb602c5074ee8751705310
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-06-06 16:53:05 -0300

    '@v1.1.48'

--- Jordan-web-app/jordan/src/components/jordan/pages/welcome/welcome.tsx
+++ Jordan-web-app/jordan/src/components/jordan/pages/welcome/welcome.tsx
@@ -4,13 +4,12 @@ import "../../../../App.css";
 import Logo from "../../../../assets/jordan/welcome/x-logo.png";
 
 import Icon from "../../../props/icon";
-import Spinner from "../../../props/spinner";
+
 import { useNavigate } from "react-router-dom";
 
 import { animation } from "../../../../services/jordan/welcome/service";
 
 function Welcome() {
-
   let duration: number = 3000;
 
   const changeDirectory = useNavigate();
@@ -19,17 +18,12 @@ function Welcome() {
 
   animation(duration, navigate);
 
-  return (  
-          <div className="welcome">
-            
-            <div className="title">
-                
-              {/* componente logo */}
-              <Icon className="logo" alt="logo" src={Logo} />
-
-            </div>
-
-          </div>
+  return (
+    <div className="welcome">
+      <div className="title">
+        <Icon className="logo" alt="logo" src={Logo} />
+      </div>
+    </div>
   );
 }
 
